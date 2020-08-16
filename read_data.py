6783581912de2eece80bea96c915e44389048b7d
tmorris <morris.trey.j@gmail.com>
2020-08-16 10:39:49 -0400

    Focus on just processing sell_price data

--- read_data.py
+++ read_data.py
@@ -10,12 +10,12 @@ def read_data(file):
         print(price_data.head)
     else:
         comp_data = pd.read_csv(file)
-        print(comp_data.head())
+        #print(comp_data.head())
 
 def output_prediction(file):
     print('You are submitting data')
-    submission = pd.read_csv(file)
-    print(submission.head())
+    #submission = pd.read_csv(file)
+    #print(submission.head())
 
 def read_training_data(file):
     if 'val' in file:
@@ -24,15 +24,15 @@ def read_training_data(file):
         read_training_eval_data(file)
     else:
         print('You are reading other data')
-        training_df = pd.read_csv(file)
-        print(training_df.head())
+        #training_df = pd.read_csv(file)
+        #print(training_df.head())
 
 def read_training_eval_data(file):
     print('You are reading evaluation training data')
-    training__eval_df = pd.read_csv(file)
-    print(training__eval_df.head())
+    #training__eval_df = pd.read_csv(file)
+    #print(training__eval_df.head())
 
 def read_training_val_data(file):
     print('You are reading validation data')
-    training_val_df = pd.read_csv(file)
-    print(training_val_df.head())
+    #training_val_df = pd.read_csv(file)
+    #print(training_val_df.head())
